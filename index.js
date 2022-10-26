7e64514bbc91cbc9fc8d3ef93b0c31eba175dc71
mlgyw <kirya.gritsyk@mail.ru>
2022-10-26 11:31:03 +0300

    refactoring

--- index.js
+++ index.js
@@ -2,37 +2,19 @@ import * as dotenv from 'dotenv';
 import fetch from 'node-fetch';
 import Express  from 'express';
 
-const app = Express();
 dotenv.config();
 
+const app = Express();
 const port = process.env.PORT;
-const hostname = process.env.HOSTNAME;
 const key = process.env.KEY;
 const api = 'https://api.nasa.gov/neo/rest/v1/feed?start_date=2022-10-10&end_date=2022-10-17&api_key=' + key;
-
 
 app.get('/meteors', async (req, res)=>{
   const nasa = await fetch(api)
   const data = await nasa.json();
   res.send(data);
-})//near_earth_objects
-
-
-app.listen(port, ()=>{
-  console.log(`Server running at http://${hostname}:${port}/`);
 })
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.listen(port, ()=>{
+  console.log(`Server running at http://localhost:${port}/`);
+})
