8c0101e8aa1b1a8432dacf9db8285a8a08241439
mlgyw <kirya.gritsyk@mail.ru>
2022-11-04 13:20:22 +0300

    unfinished

--- index.js
+++ index.js
@@ -12,23 +12,24 @@ const api = 'https://api.nasa.gov/neo/rest/v1/feed?start_date=2022-10-10&end_dat
 app.get('/meteors', async (req, res)=>{
   const nasa = await fetch(api)
   const data = await nasa.json();
-  let result={}
+  let result=[]
   let arr =[]
-  for(let item in data['near_earth_objects']){
-   for (let i = 0; i < data['near_earth_objects'][item].length; i++) {
-    result.id = data['near_earth_objects'][item][i].id
-    result.name=data['near_earth_objects'][item][i].name
-    result.diameter = data['near_earth_objects'][item][i]['estimated_diameter']['meters']//['estimated_diameter_min']
-    result.hazard =data['near_earth_objects']['2022-10-14'][i]?.is_potentially_hazardous_asteroid
-    result.approach =data['near_earth_objects'][item][i]['close_approach_data']
-    result.relative = data['near_earth_objects'][item][i]['close_approach_data'][0]['relative_velocity']['kilometers_per_hour']
-   
-    arr.push(result)
-    result={};
-    }  }
-  console.log(arr)
-  // res.send(data);
-})
+  let prop = ["id","name",'meters',"is_potentially_hazardous_asteroid",'kilometers_per_hour']
+
+  function itog(ca) {
+    Object.entries(ca).forEach(([key,value]) => {
+      if(typeof value == "object"){
+        itog(value)
+      }
+      if(prop.includes(key)){
+        result.push({key,value})
+      }
+    });
+  }
+  
+  (itog(data.near_earth_objects))
+  console.log(result);
+  })
 
 app.listen(port, ()=>{
   console.log(`Server running at http://localhost:${port}/`);
