41159b116374979d7836fdf54d69dc4610a61299
mlgyw <kirya.gritsyk@mail.ru>
2022-11-08 12:34:03 +0300

    modify

--- index.js
+++ index.js
@@ -10,26 +10,32 @@ const key = process.env.KEY;
 const url = process.env.URL
 const api = url + key;
 
-let props = ["id","name",'meters',"is_potentially_hazardous_asteroid",'kilometers_per_hour','kilometers_per_hour','close_approach_date_full']
-let result=[]
+
 
 app.get('/meteors', async (req, res)=>{
   const nasa = await fetch(api)
   const data = await nasa.json();
-  
+  let props = ["id","name",'meters',"is_potentially_hazardous_asteroid",'kilometers_per_hour','close_approach_date_full']
+  let result=[]
+
   let getMeteorsData = (receivedData) => {
     Object.entries(receivedData).forEach(([key,value]) => {
       if(typeof value == "object"){
         getMeteorsData(value)
       }
       if(props.includes(key)){
-        result.push({key,value})
+        if(key=="id"){
+        result.push({})
       }
+        result[result.length-1][key]=value;
+      }
+      
     });
   }
 
   getMeteorsData(data.near_earth_objects)
   res.send(result);
+  console.log(result)
   })
 
  
